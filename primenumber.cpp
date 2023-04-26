f66713fd3f021a841c476c1466a9ef93d4cd3c80
Akshat Tyagi <91817453+Akshattyagi123@users.noreply.github.com>
2023-04-26 22:41:37 +0530

    Add files via upload
    finding whether number is prime or not

---
+++ primenumber.cpp
@@ -0,0 +1,34 @@
+// complexity will be O(n)
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        cout << "It is not a prime number" << endl;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (isPrime(n))
+    {
+        cout << "It is a prime number" << endl;
+    }
+    else
+    {
+        cout << "It is not a prime number" << endl;
+    }
+}
