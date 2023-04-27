165d1caaf3319ed673639a88715887a8d1158fc4
Akshat Tyagi <91817453+Akshattyagi123@users.noreply.github.com>
2023-04-27 21:01:38 +0530

    Add files via upload

---
+++ Modular exponentiation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+int modularExponentiation(int x, int n, int m)
+{
+    // Write your code here.
+    int res = 1;
+
+    while (n > 0)
+    {
+        if (n & 1)
+        {
+            // odd
+            res = (1LL * (res) * (x)) % m;
+        }
+        x = (1LL * (x) * (x)) % m;
+        n = n >> 1;
+    }
+    return res;
+}
