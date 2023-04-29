8b6b01d3f6221e465f6e96e7d9568239405ce2b8
Akshat Tyagi <91817453+Akshattyagi123@users.noreply.github.com>
2023-04-29 18:37:01 +0530

    Add files via upload
    linear search using recursion

--- Linearsearch.cpp
+++ Linearsearch.cpp
@@ -1,39 +1,46 @@
 #include <iostream>
 using namespace std;
-
-bool search(int arr[], int size, int key)
+void print(int arr[], int n)
 {
+    cout << "size of array" << n << endl;
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < n; i++)
     {
-
-        if (arr[i] == key)
-        {
-            return 1;
-        }
+        cout << arr[i] << " ";
     }
-    return 0;
+    cout << endl;
 }
-
-int main()
+bool linearSearch(int arr[], int size, int key)
 {
-
-    int arr[10] = {5, 7, -2, 10, 22, -2, 0, 5, 22, 1};
-
-    cout << " Enter the element to search for " << endl;
-    int key;
-    cin >> key;
-
-    bool found = search(arr, 10, key);
-
-    if (found)
+    print(arr, size);
+    if (size == 0)
     {
-        cout << " Key is present " << endl;
+        return false;
+    }
+    if (arr[0] == key)
+    {
+        return true;
     }
     else
     {
-        cout << " Key is absent " << endl;
+        bool remainingpart = linearSearch(arr + 1, size - 1, key);
+        return remainingpart;
     }
+}
+int main()
+{
+    int arr[5] = {3, 5, 1, 2, 6};
+    int size = 5;
+    int key = 6;
 
-    return 0;
+    bool ans = linearSearch(arr, size, key);
+
+    if (ans)
+    {
+        cout << "Present" << endl;
+    }
+    else
+    {
+        cout << "Not present" << endl;
+    }
 }
